e2020b9d22d5ec7768d34f4811f0992d58edb61c
akai <kai150036@163.com>
2024-10-28 21:47:19 +0800

    feat: 初始化项目

---
+++ pk-front-vue3/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  初始化 删除一些无用的文件
+  <RouterView />
+</template>
+
+<style scoped>
+
+
+</style>
